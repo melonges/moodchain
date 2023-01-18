067196c9c35e0fdcafd52ea07c1ec295251a05b7
melonges <mellonges@icloud.com>
2023-01-18 15:54:34 +0300

    add block validation

--- src/block.rs
+++ src/block.rs
@@ -1,13 +1,15 @@
 use chrono::{DateTime, Utc};
 use sha2::{Digest, Sha256};
 use std::time::{SystemTime, UNIX_EPOCH};
-// pub type Hash = [u8; 32];
-// todo normal export;
+
+type BlockHash = u128;
+type UnixTimeStamp = u32;
+
 pub struct Block {
     pub index: u32,
-    pub hash: String,
-    pub prev_hash: String,
-    pub timestamp: String,
+    pub hash: BlockHash,
+    pub prev_hash: BlockHash,
+    pub timestamp: UnixTimeStamp,
     pub data: String,
 }
 
@@ -15,24 +17,43 @@ impl Block {
     pub fn generate_next_block(prev_block: Block, data: String) -> Block {
         let index = prev_block.index + 1;
         let prev_hash = prev_block.hash;
-        let timestamp = SystemTime::now()
+        let timestamp: UnixTimeStamp = SystemTime::now()
             .duration_since(UNIX_EPOCH)
             .unwrap()
-            .as_secs()
-            .to_string();
+            .as_secs() as u32;
         Block {
             index,
-            hash: Block::hash(index, &prev_hash, &timestamp, &data),
+            hash: Block::hash(index, prev_hash, timestamp, &data),
             prev_hash,
             timestamp,
             data,
         }
     }
 
-    fn hash(index: u32, prev_hash: &String, timestamp: &String, data: &String) -> String {
-        let mut hasher = Sha256::new();
-        let input = format!("{}{}{}{}", index, prev_hash, timestamp, data);
-        hasher.update(input.as_bytes());
-        format!("{:x}", hasher.finalize())
+    fn hash(
+        index: u32,
+        prev_hash: BlockHash,
+        timestamp: UnixTimeStamp,
+        data: &String,
+    ) -> BlockHash {
+        // TODO make normal hashing
+        (prev_hash ^ (timestamp + index) as BlockHash)
+    }
+
+    fn validate_new_block(new_block: Block, prev_block: Block) -> bool {
+        if prev_block.index + 1 != new_block.index {
+            return false;
+        } else if prev_block.hash != new_block.prev_hash {
+            return false;
+        } else if Block::hash(
+            new_block.index,
+            new_block.hash,
+            new_block.timestamp,
+            &new_block.data,
+        ) != new_block.hash
+        {
+            return false;
+        }
+        true
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
 mod block;
+use block::Block;
 fn main() {
-    let a = ['a'; 32];
+    let blockchain = vec![Block {
+        index: 0,
+        prev_hash: 0x0,
+        data: "GENESIS_BLOCK".to_string(),
+        timestamp: 1674044792,
+        hash: 0x985ed1d54a9f4bdc815eb5d41354ef67,
+    }];
+
     println!("Hello, world!");
 }
